8053b1517dc9b437c907567c5bca9c31116679fa
Zachary Foster <zfoster22@gmail.com>
2018-02-22 12:10:35 -0800

    Ability to add new recipe with name, notes
    - present add/edit VC modally. User can Cancel or Save. Saving without a
      name == Cancel

--- Baking Tools/AddEditRecipeViewController.swift
+++ Baking Tools/AddEditRecipeViewController.swift
@@ -8,7 +8,42 @@
 
 import UIKit
 import Foundation
+import CoreData
 
 class AddEditRecipeViewController : UIViewController {
     
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var notes: UITextView!
+    
+    @IBAction func tapSave(_ sender: UIBarButtonItem) {
+        guard let nameText = name.text, !nameText.isEmpty else {
+            self.tapCancel(sender)
+            return
+        }
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let context = delegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "RecipeModel", in: context)
+        let newRecipe = NSManagedObject(entity: entity!, insertInto: context)
+        
+        newRecipe.setValue(nameText, forKey: "name")
+        newRecipe.setValue(notes.text, forKey: "notes")
+        
+        do {
+            try context.save()
+        } catch {
+            print("Failed saving")
+        }
+        
+        self.dismiss(animated: true) {
+            
+        }
+    }
+    
+    @IBAction func tapCancel(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true) {
+            
+        }
+    }
 }
--- Baking Tools/RecipesViewController.swift
+++ Baking Tools/RecipesViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 class RecipesViewController : UITableViewController, NSFetchedResultsControllerDelegate {
-    
+        
     var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>!
 
     override func viewDidLoad() {
