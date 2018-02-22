fa17a10d6106edbb2662396089ed57015b1f004b
Zachary Foster <zfoster22@gmail.com>
2018-02-22 14:06:19 -0800

    add add ingredient modal

---
+++ Baking Tools/AddIngredientViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AddIngredientViewController.swift
+//  Baking Tools
+//
+//  Created by Zachary Foster on 2/22/18.
+//  Copyright © 2018 Zachary Foster. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreData
+
+class AddIngredientViewController : UIViewController {
+    
+    @IBAction func tapSave(_ sender: Any) {
+        self.dismiss(animated: true) {
+            
+        }
+    }
+    
+    @IBAction func tapCancel(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true) {
+            
+        }
+    }
+}
